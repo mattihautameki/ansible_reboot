5d9d83a030fe4cc7b6ad1c5b77230418b6004de4
Markus Anglmaier <markus@anglmaier.at>
2017-02-28 09:57:45 +0100

    Added the optional ability to check a tcp port istead of icmp echo requests

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Ansible Reboot helper
 
-This script can be executed as local action task after a host was sent the reboot command. The script checks the host constantly every 2 seconds and returns with exitcode 0 when the host was online, went offline and comes back online. Simple Pings are used to check the host. 
+This script can be executed as local action task after a host was sent the reboot command. The script checks the host constantly every 2 seconds and returns with exitcode 0 when the host was online, went offline and comes back online. Either pings or connections to tcp ports are used to check the hosts status. 
--- check_reboot.py
+++ check_reboot.py
@@ -5,6 +5,7 @@ import os
 import time
 import sys
 import re
+import socket
 
 
 parser = argparse.ArgumentParser(description='Script ueberprueft einen Host ob dieser rebootet hat.')
@@ -12,6 +13,8 @@ parser.add_argument('hostname', metavar='HOST', type=str,
                    help='Welcher Host soll abgefragt werden')
 parser.add_argument('-t', dest='timeout', metavar='timeout', type=int, default=60,
                    help='Wie lange soll versucht werden einen Reboot zu erkennen.\n defaults = 60')
+parser.add_argument('-p', dest='port', metavar='port', type=int,
+                   help='Checke TCP Port anstatt icmp echo reply.')
 args = parser.parse_args()
 
 
@@ -21,11 +24,12 @@ start_time = int(time.time())
 
 
 class Hoststate():
-  def __init__(self, host):
+  def __init__(self, host, port = None):
       self.hostname = host 
       self.state = None
       self.state_list = []
       self.rebooted = None
+      self.port = port
 
   def isRebooted(self):
       if re.match(".*1[0]{2,}11.*", "".join(self.state_list)):
@@ -36,16 +40,25 @@ class Hoststate():
           return False
 
   def check(self):
-      with open(os.devnull, 'w') as devnull:
-          try:
-              self.__p = subprocess.check_call(['ping', '-w', '1', '-c', '1', self.hostname], stdout=devnull, stderr=devnull)
-          except subprocess.CalledProcessError:
+      if self.port: 
+          try: 
+              s = socket.socket()
+              s.connect((self.hostname, self.port))
+              self.__p = 0
+          except socket.error as e:
               self.__p = None
+      else:        
+          with open(os.devnull, 'w') as devnull:
+              try:
+                  self.__p = subprocess.check_call(['ping', '-w', '1', '-c', '1', self.hostname], stdout=devnull, stderr=devnull)
+              except subprocess.CalledProcessError:
+                  self.__p = None
+
       self.state =  1 if self.__p == 0 else 0
       self.state_list.append(str(1) if self.__p == 0 else str(0))
 
 
-server=Hoststate(args.hostname)
+server=Hoststate(args.hostname, args.port)
 rc = None
 
 try:
